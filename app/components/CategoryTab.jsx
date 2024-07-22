b984dcee108dd528fec1356d17d85d1975ce8352
Mert Öztürk <mert32139@gmail.com>
2024-07-23 02:38:05 +0300

    tab menü içerik ayarlamaları yapıldı

--- app/components/CategoryTab.jsx
+++ app/components/CategoryTab.jsx
@@ -11,3 +11,261 @@ export const categoryTabReducer = (state, action) => {
     selectedTab: 'Tab1',
   };
   
+
+  export   const tabs = [
+    { id: 'Tab1', head:"Akademik Destek", label: [
+        {
+            img:'/ogretmens.jpg',
+            title: "Öğretmenler",
+            desc: "Öğretmenlerimize ulaşmak için tıklayınız"
+        },
+        {
+            img:'/ogretmens.jpg',
+            title: "Öğretmenler",
+            desc: "Öğretmenlerimize ulaşmak için tıklayınız"
+        },
+        {
+            img:'/ogretmens.jpg',
+            title: "Öğretmenler",
+            desc: "Öğretmenlerimize ulaşmak için tıklayınız"
+        },
+        {
+            img:'/ogretmens.jpg',
+            title: "Öğretmenler",
+            desc: "Öğretmenlerimize ulaşmak için tıklayınız"
+        },
+        {
+            img:'/ogretmens.jpg',
+            title: "Öğretmenler",
+            desc: "Öğretmenlerimize ulaşmak için tıklayınız"
+        },
+    ]
+},
+    
+
+{ id: 'Tab2', head:"Takviye", label: [
+    {
+        img:'/ogretmens.jpg',
+        title: "Öğretmenler 1",
+        desc: "Öğretmenlerimize ulaşmak için tıklayınız"
+    },
+    {
+        img:'/ogretmens.jpg',
+        title: "Öğretmenler 2 ",
+        desc: "Öğretmenlerimize ulaşmak için tıklayınız"
+    },
+    {
+        img:'/ogretmens.jpg',
+        title: "Öğretmenler 3",
+        desc: "Öğretmenlerimize ulaşmak için tıklayınız"
+    },
+    {
+        img:'/ogretmens.jpg',
+        title: "Öğretmenler 4",
+        desc: "Öğretmenlerimize ulaşmak için tıklayınız"
+    },
+    {
+        img:'/ogretmens.jpg',
+        title: "Öğretmenler 5",
+        desc: "Öğretmenlerimize ulaşmak için tıklayınız"
+    },
+]
+},
+
+    { id: 'Tab3', head:"Sanat ve Yaratıcılık",  label: [
+        {
+            img:'/ogretmens.jpg',
+            title: "Öğretmenler 1",
+            desc: "Öğretmenlerimize ulaşmak için tıklayınız"
+        },
+        {
+            img:'/ogretmens.jpg',
+            title: "Öğretmenler 2 ",
+            desc: "Öğretmenlerimize ulaşmak için tıklayınız"
+        },
+        {
+            img:'/ogretmens.jpg',
+            title: "Öğretmenler 3",
+            desc: "Öğretmenlerimize ulaşmak için tıklayınız"
+        },
+        {
+            img:'/ogretmens.jpg',
+            title: "Öğretmenler 4",
+            desc: "Öğretmenlerimize ulaşmak için tıklayınız"
+        },
+        {
+            img:'/ogretmens.jpg',
+            title: "Öğretmenler 5",
+            desc: "Öğretmenlerimize ulaşmak için tıklayınız"
+        },
+    ]},
+
+    { id: 'Tab4', head:"Yabancı Dil", label: [
+        {
+            img:'/ogretmens.jpg',
+            title: "Öğretmenler 1",
+            desc: "Öğretmenlerimize ulaşmak için tıklayınız"
+        },
+        {
+            img:'/ogretmens.jpg',
+            title: "Öğretmenler 2 ",
+            desc: "Öğretmenlerimize ulaşmak için tıklayınız"
+        },
+        {
+            img:'/ogretmens.jpg',
+            title: "Öğretmenler 3",
+            desc: "Öğretmenlerimize ulaşmak için tıklayınız"
+        },
+        {
+            img:'/ogretmens.jpg',
+            title: "Öğretmenler 4",
+            desc: "Öğretmenlerimize ulaşmak için tıklayınız"
+        },
+        {
+            img:'/ogretmens.jpg',
+            title: "Öğretmenler 5",
+            desc: "Öğretmenlerimize ulaşmak için tıklayınız"
+        },
+    ]
+
+},
+    { id: 'Tab5',head:"Sınav Hazırlık Kursları", label:[
+        {
+            img:'/ogretmens.jpg',
+            title: "Öğretmenler 1",
+            desc: "Öğretmenlerimize ulaşmak için tıklayınız"
+        },
+        {
+            img:'/ogretmens.jpg',
+            title: "Öğretmenler 2 ",
+            desc: "Öğretmenlerimize ulaşmak için tıklayınız"
+        },
+        {
+            img:'/ogretmens.jpg',
+            title: "Öğretmenler 3",
+            desc: "Öğretmenlerimize ulaşmak için tıklayınız"
+        },
+        {
+            img:'/ogretmens.jpg',
+            title: "Öğretmenler 4",
+            desc: "Öğretmenlerimize ulaşmak için tıklayınız"
+        },
+        {
+            img:'/ogretmens.jpg',
+            title: "Öğretmenler 5",
+            desc: "Öğretmenlerimize ulaşmak için tıklayınız"
+        },
+    ]
+},
+    { id: 'Tab6', head:"Spor ve Fiziksel Aktivite", label:[
+        {
+            img:'/ogretmens.jpg',
+            title: "Öğretmenler 1",
+            desc: "Öğretmenlerimize ulaşmak için tıklayınız"
+        },
+        {
+            img:'/ogretmens.jpg',
+            title: "Öğretmenler 2 ",
+            desc: "Öğretmenlerimize ulaşmak için tıklayınız"
+        },
+        {
+            img:'/ogretmens.jpg',
+            title: "Öğretmenler 3",
+            desc: "Öğretmenlerimize ulaşmak için tıklayınız"
+        },
+        {
+            img:'/ogretmens.jpg',
+            title: "Öğretmenler 4",
+            desc: "Öğretmenlerimize ulaşmak için tıklayınız"
+        },
+        {
+            img:'/ogretmens.jpg',
+            title: "Öğretmenler 5",
+            desc: "Öğretmenlerimize ulaşmak için tıklayınız"
+        },
+    ]
+},
+    { id: 'Tab7',  head:"Sağlık ve Psikolojik Destek", label: [
+        {
+            img:'/ogretmens.jpg',
+            title: "Öğretmenler 1",
+            desc: "Öğretmenlerimize ulaşmak için tıklayınız"
+        },
+        {
+            img:'/ogretmens.jpg',
+            title: "Öğretmenler 2 ",
+            desc: "Öğretmenlerimize ulaşmak için tıklayınız"
+        },
+        {
+            img:'/ogretmens.jpg',
+            title: "Öğretmenler 3",
+            desc: "Öğretmenlerimize ulaşmak için tıklayınız"
+        },
+        {
+            img:'/ogretmens.jpg',
+            title: "Öğretmenler 4",
+            desc: "Öğretmenlerimize ulaşmak için tıklayınız"
+        },
+        {
+            img:'/ogretmens.jpg',
+            title: "Öğretmenler 5",
+            desc: "Öğretmenlerimize ulaşmak için tıklayınız"
+        },
+    ]
+},
+    { id: 'Tab8',  head:"Kişisel Gelişim ve Beceriler",  label: [
+        {
+            img:'/ogretmens.jpg',
+            title: "Öğretmenler 1",
+            desc: "Öğretmenlerimize ulaşmak için tıklayınız"
+        },
+        {
+            img:'/ogretmens.jpg',
+            title: "Öğretmenler 2 ",
+            desc: "Öğretmenlerimize ulaşmak için tıklayınız"
+        },
+        {
+            img:'/ogretmens.jpg',
+            title: "Öğretmenler 3",
+            desc: "Öğretmenlerimize ulaşmak için tıklayınız"
+        },
+        {
+            img:'/ogretmens.jpg',
+            title: "Öğretmenler 4",
+            desc: "Öğretmenlerimize ulaşmak için tıklayınız"
+        },
+        {
+            img:'/ogretmens.jpg',
+            title: "Öğretmenler 5",
+            desc: "Öğretmenlerimize ulaşmak için tıklayınız"
+        },
+    ]},
+    { id: 'Tab9', head:"Kariyer ve Gelecek Planlama", label: [
+        {
+            img:'/ogretmens.jpg',
+            title: "Öğretmenler 1",
+            desc: "Öğretmenlerimize ulaşmak için tıklayınız"
+        },
+        {
+            img:'/ogretmens.jpg',
+            title: "Öğretmenler 2 ",
+            desc: "Öğretmenlerimize ulaşmak için tıklayınız"
+        },
+        {
+            img:'/ogretmens.jpg',
+            title: "Öğretmenler 3",
+            desc: "Öğretmenlerimize ulaşmak için tıklayınız"
+        },
+        {
+            img:'/ogretmens.jpg',
+            title: "Öğretmenler 4",
+            desc: "Öğretmenlerimize ulaşmak için tıklayınız"
+        },
+        {
+            img:'/ogretmens.jpg',
+            title: "Öğretmenler 5",
+            desc: "Öğretmenlerimize ulaşmak için tıklayınız"
+        },
+    ]
+},
+  ];
