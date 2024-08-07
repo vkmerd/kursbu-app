d2b3cecae65c3cfa3805609b4e4cc3e6a64cebd4
Mert Öztürk <mert32139@gmail.com>
2024-08-08 01:13:05 +0300

    deploy hatası giderildi

--- app/api/auth/register/route.js
+++ app/api/auth/register/route.js
@@ -1,22 +1,22 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
+// import { PrismaClient } from '@prisma/client';
+// const prisma = new PrismaClient();
 
-export async function POST(req) {
-  const { name, surname, email, password, role, phone, photo } = await req.json();
+// export async function POST(req) {
+//   const { name, surname, email, password, role, phone, photo } = await req.json();
 
-  try {
-    const newUser = await prisma.user.create({
-      data: {
-        email,
-        password,
-        role,
-        student: role === 'STUDENT' ? { create: { name, surname, phone, photo } } : undefined,
-        teacher: role === 'TEACHER' ? { create: { name, surname, phone, photo } } : undefined,
-      },
-    });
+//   try {
+//     const newUser = await prisma.user.create({
+//       data: {
+//         email,
+//         password,
+//         role,
+//         student: role === 'STUDENT' ? { create: { name, surname, phone, photo } } : undefined,
+//         teacher: role === 'TEACHER' ? { create: { name, surname, phone, photo } } : undefined,
+//       },
+//     });
 
-    return new Response(JSON.stringify(newUser), { status: 201 });
-  } catch (error) {
-    return new Response(JSON.stringify({ error: 'Error creating user' }), { status: 500 });
-  }
-}
+//     return new Response(JSON.stringify(newUser), { status: 201 });
+//   } catch (error) {
+//     return new Response(JSON.stringify({ error: 'Error creating user' }), { status: 500 });
+//   }
+// }
